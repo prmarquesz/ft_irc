6dd1eed696abfd3f2a092847a66254fd3bc85260
Paulo <paulormarques21@gmail.com>
2023-10-15 18:53:02 -0300

    feat: server client

---
+++ include/Client.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include <Channel.hpp>
+
+#define BUFFER_SIZE 512
+
+#define PASS_FLAG 2
+#define USER_FLAG 4
+#define NICK_FLAG 8
+
+#define OPER_USER "foo"
+#define OPER_PASS "bar"
+
+#define CLI_OPER 0b0001
+#define CLI_INV 0b0010
+#define CLI_WALLOP 0b0100
+#define CLI_NOTICE 0b1000
+
+class Channel;
+
+class Client {
+	private:
+		int			 			fd;
+		int			 			registration;	
+		bool		 			welcome;		
+		bool		 			knowPassword;
+		bool		 			toDisconnect;
+		unsigned int 			flags;
+		std::string	 			nickname;
+		std::string	 			username;
+		std::string	 			hostname;
+		std::string	 			servername;
+		std::string	 			realname;
+		std::string	 			sendData;
+		std::vector<Channel *>	channels;
+
+		bool 					setOperator(bool on);
+		bool 					setInvisible(bool on);
+		bool 					setNotice(bool on);
+		bool 					setWallop(bool on);
+
+	public:
+		~Client(void);
+		Client(void);
+		Client(int fd, std::string hostname);
+
+		void 					setSendData(std::string data);
+		void 					setHostname(std::string name);
+		void 					setServername(std::string name);
+		void 					setUsername(std::string name);
+		void 					setRealname(std::string name);
+		void 					setNickname(std::string name);
+		bool 					setMode(char mode, bool on);
+		void 					setRegistration(int flag);
+		void 					setOp(bool value);
+		void 					setKnowPassword(bool value);
+		void 					setToDisconnect(bool value);
+		void 					setWelcome(bool value);
+		void 					resetSendData(int len);
+		void 					addChannel(Channel &ch);
+		void 					removeChannel(Channel &ch);
+
+		bool					isVisible() const;
+		int						getFd() const;
+		int						getRegistration() const;
+		bool					getWelcome() const;
+		bool					getOp() const;
+		bool					getToDisconnect() const;
+		std::string				getNickname() const;
+		std::string				getUsername() const;
+		std::string				getHostname() const;
+		std::string				getServername() const;
+		std::string				getRealname() const;
+		std::string				getSendData() const;
+		std::vector<Channel *>	&getChannels();
+		std::string				getModesStr() const;
+
+		std::string 			getClientPrefix();
+};
---
+++ src/Client.cpp
@@ -0,0 +1,200 @@
+#include <Client.hpp>
+
+Client::~Client() {
+	std::size_t i = 0;
+	while (i < channels.size()) {
+		channels.at(i)->removeClient((*this));
+		i++;
+	}
+}
+
+Client::Client() {
+	this->fd		   = 0;
+	this->registration = 0;
+	this->flags		   = CLI_INV | CLI_WALLOP | CLI_NOTICE;
+	this->knowPassword = false;
+	this->welcome	   = false;
+	this->toDisconnect = false;
+}
+
+Client::Client(int fd, std::string hostname) {
+	this->fd		   = fd;
+	this->registration = 0;
+	this->flags		   = CLI_INV | CLI_WALLOP | CLI_NOTICE;
+	this->knowPassword = false;
+	this->welcome	   = false;
+	this->toDisconnect = false;
+	this->hostname	   = hostname;
+}
+
+int			Client::getFd() const { return fd; }
+
+int			Client::getRegistration() const { return registration; }
+
+bool		Client::getWelcome() const { return welcome; }
+
+bool		Client::getToDisconnect() const { return toDisconnect; }
+
+bool		Client::getOp() const { return flags & CLI_OPER; }
+
+std::string Client::getNickname() const { return nickname; }
+
+std::string Client::getUsername() const { return username; }
+
+std::string Client::getHostname() const { return hostname; }
+
+std::string Client::getSendData() const { return sendData; }
+
+std::string Client::getServername() const { return servername; }
+
+std::string Client::getRealname() const { return realname; }
+
+std::string Client::getClientPrefix() {
+	std::stringstream ss;
+
+	ss << ":" << getNickname();
+	ss << "!" << getUsername();
+	ss << "@" << getHostname();
+	return (ss.str());
+};
+
+void Client::setOp(bool value) {
+	if (value)
+		flags |= CLI_OPER;
+	else
+		flags = flags & ~CLI_OPER;
+}
+
+void Client::setWelcome(bool value) { welcome = value; }
+
+void Client::setKnowPassword(bool value) { knowPassword = value; }
+
+void Client::setRegistration(int flag) { registration |= flag; }
+
+void Client::setUsername(std::string name) { username = name; }
+
+void Client::setHostname(std::string name) { hostname = name; }
+
+void Client::setRealname(std::string name) { realname = name; }
+
+void Client::setServername(std::string name) { servername = name; }
+
+void Client::setSendData(std::string data) { sendData += data; }
+
+void Client::setToDisconnect(bool value) { toDisconnect = value; }
+
+void Client::setNickname(std::string name) {
+	if (name.length() > 9) {
+		nickname = name.substr(0, 9);
+	} else {
+		nickname = name;
+	}
+}
+
+void Client::resetSendData(int len) { sendData = sendData.substr(len); }
+
+void Client::removeChannel(Channel &ch) {
+	channels.erase(find(channels.begin(), channels.end(), &ch));
+}
+
+std::vector<Channel *> &Client::getChannels() { return channels; }
+
+void Client::addChannel(Channel &ch) { channels.push_back(&ch); };
+
+bool Client::setOperator(bool on) {
+	unsigned int before = flags;
+
+	if (on) {
+		flags |= CLI_OPER;
+	} else {
+		flags = flags & ~CLI_OPER;
+	}
+	if (flags == before) {
+		return false;
+	}
+	return true;
+};
+
+bool Client::setInvisible(bool on) {
+	unsigned int before = flags;
+
+	if (on) {
+		flags |= CLI_INV;
+	} else {
+		flags = flags & ~CLI_INV;
+	}
+	if (flags == before) {
+		return false;
+	}
+	return true;
+};
+
+bool Client::setNotice(bool on) {
+	unsigned int before = flags;
+
+	if (on) {
+		flags |= CLI_NOTICE;
+	} else {
+		flags = flags & ~CLI_NOTICE;
+	}
+	if (flags == before) {
+		return false;
+	}
+	return true;
+};
+
+bool Client::setWallop(bool on) {
+	unsigned int before = flags;
+
+	if (on) {
+		flags |= CLI_WALLOP;
+	} else {
+		flags = flags & ~CLI_WALLOP;
+	}
+	if (flags == before) {
+		return false;
+	}
+	return true;
+};
+
+bool Client::setMode(char mode, bool on) {
+	bool changed = false;
+	switch (mode) {
+		case 'i':
+			changed = this->setInvisible(on);
+			break;
+		case 'w':
+			changed = this->setWallop(on);
+			break;
+		case 's':
+			changed = this->setNotice(on);
+			break;
+		case 'o':
+			changed = this->setOperator(on);
+			break;
+		default:
+			break;
+	}
+	return changed;
+};
+
+std::string Client::getModesStr() const {
+	std::string modes = "";
+
+	if (CLI_OPER & flags) {
+		modes.append(1, 'o');
+	}
+	if (CLI_INV & flags) {
+		modes.append(1, 'i');
+	}
+	if (CLI_WALLOP & flags) {
+		modes.append(1, 'w');
+	}
+	if (CLI_NOTICE & flags) {
+		modes.append(1, 's');
+	}
+	if (modes.size() > 0) modes.insert(0, 1, '+');
+	return modes;
+};
+
+bool Client::isVisible() const { return !(this->flags & CLI_INV); };
